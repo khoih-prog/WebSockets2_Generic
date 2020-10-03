4f5b68274e30750b791eff7f8e79a014be454d51
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-10-03 03:12:28 -0400

    v1.0.7
    ### Releases v1.0.7
    
    1. Add support to ENC28J60 Ethernet using **EthernetENC**.
    2. Add Ethernet Library Patches for **UIPEthernet v2.0.9 and Ethernet2**
    3. Update Platform.ini to support **PlatformIO 5.x owner-based dependency declaration**.

--- examples/ESP8266/Esp8266-AdvancedWebServer/Esp8266-AdvancedWebServer.ino
+++ examples/ESP8266/Esp8266-AdvancedWebServer/Esp8266-AdvancedWebServer.ino
@@ -12,18 +12,19 @@
 
   Built by Khoi Hoang https://github.com/khoih-prog/Websockets2_Generic
   Licensed under MIT license
-  Version: 1.0.6
+  Version: 1.0.7
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
   1.0.0   K Hoang      14/07/2020 Initial coding/porting to support nRF52 and SAMD21/SAMD51 boards. Add SINRIC/Alexa support
   1.0.1   K Hoang      16/07/2020 Add support to Ethernet W5x00 to nRF52, SAMD21/SAMD51 and SAM DUE boards
   1.0.2   K Hoang      18/07/2020 Add support to Ethernet ENC28J60 to nRF52, SAMD21/SAMD51 and SAM DUE boards
-  1.0.3   K Hoang      18/07/2020 Add support to STM32F boards using Ethernet W5x00, ENC28J60 and LAN8742A
-  1.0.4   K Hoang      27/07/2020 Add support to STM32F/L/H/G/WB/MP1 and Seeeduino SAMD21/SAMD51 using
+  1.0.3   K Hoang      18/07/2020 Add support to STM32F boards using Ethernet W5x00, ENC28J60 and LAN8742A 
+  1.0.4   K Hoang      27/07/2020 Add support to STM32F/L/H/G/WB/MP1 and Seeeduino SAMD21/SAMD51 using 
                                   Ethernet W5x00, ENC28J60, LAN8742A and WiFiNINA. Add examples and Packages' Patches.
   1.0.5   K Hoang      29/07/2020 Sync with ArduinoWebsockets v0.4.18 to fix ESP8266 SSL bug.
-  1.0.6   K Hoang      06/08/2020 Add non-blocking WebSocketsServer feature and non-blocking examples.
+  1.0.6   K Hoang      06/08/2020 Add non-blocking WebSocketsServer feature and non-blocking examples.       
+  1.0.7   K Hoang      03/10/2020 Add support to Ethernet ENC28J60 using EthernetENC and UIPEthernet v2.0.9
  *****************************************************************************************************************************/
 
 #include "defines.h"
