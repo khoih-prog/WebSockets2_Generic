4937eb7f01be491f326ca82dd8c3a1298c4f21c0
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-05-25 02:35:33 -0400

    v1.4.0 to add support to RP2040-based boards
    ### Major Release v1.4.0
    
    1. Add support to RP2040-based boards, such as **RASPBERRY_PI_PICO, ADAFRUIT_FEATHER_RP2040 and GENERIC_RP2040**, using [**Earle Philhower's arduino-pico** v1.5.1+ core](https://github.com/earlephilhower/arduino-pico).
    2. Add support to RP2040-based boards, such as **RASPBERRY_PI_PICO, ADAFRUIT_FEATHER_RP2040 and GENERIC_RP2040**, using [**Arduino-mbed RP2040** v2.1.0+ core](https://github.com/arduino/ArduinoCore-mbed).
    4. Add examples with new features

---
+++ examples/ESP8266/Esp8266-Server/defines.h
@@ -0,0 +1,39 @@
+/****************************************************************************************************************************
+  defines.h for ESP8266-Client.ino
+  For ESP8266.
+  
+  Based on and modified from Gil Maimon's ArduinoWebsockets library https://github.com/gilmaimon/ArduinoWebsockets
+  to support STM32F/L/H/G/WB/MP1, nRF52 and SAMD21/SAMD51 boards besides ESP8266 and ESP32
+
+
+  The library provides simple and easy interface for websockets (Client and Server).
+  
+  Built by Khoi Hoang https://github.com/khoih-prog/Websockets2_Generic
+  Licensed under MIT license
+ *****************************************************************************************************************************/
+
+#ifndef defines_h
+#define defines_h
+
+#if (ESP8266)
+  #define BOARD_TYPE      "ESP8266"
+#else
+  #error This code is intended to run only on the ESP8266 boards ! Please check your Tools->Board setting.
+#endif
+
+#ifndef BOARD_NAME
+  #define BOARD_NAME    BOARD_TYPE
+#endif
+
+#define DEBUG_WEBSOCKETS_PORT     Serial
+// Debug Level from 0 to 4
+#define _WEBSOCKETS_LOGLEVEL_     3
+
+const char* ssid = "ssid"; //Enter SSID
+const char* password = "password"; //Enter Password
+
+#define WEBSOCKETS_PORT     8080
+
+const uint16_t websockets_server_port = WEBSOCKETS_PORT; // Enter server port
+
+#endif      //defines_h
